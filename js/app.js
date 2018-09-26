69d7cd7b78f450578919c9aa999595263f32962f
MiaTran <ttdiemquyen@live.com>
2018-09-26 23:11:45 +1000

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).foundation();
+
+
+
+$(function() {
+  $('.search')
+    .bind('click', function(event) {
+      $(".search-field").toggleClass("expand-search");
+
+      // if the search field is expanded, focus on it
+      if ($(".search-field").hasClass("expand-search")) {
+        $(".search-field").focus();
+      }
+    })
+});
+
+
